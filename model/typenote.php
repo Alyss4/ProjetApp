a159e8cf1cca3a6055230c570ca5f2d8dec050bf
Alyssa FRIEDRICH <afriedrich@ESTIC.ADMIN>
2024-01-26 14:17:14 +0100

    Création du model typenote

--- model/typenote.php
+++ model/typenote.php
@@ -1,16 +1,16 @@
 <?php
-class Competence{
-    private $pdo;
+class typeNote{
+    private $pdo; 
     public function __construct(){
-    $config = parse_ini_file("config.ini");
+        $config = parse_ini_file("config.ini");
         try{
             $this->pdo = new \PDO("mysql:host=".$config["host"].";dbname=".$config["database"].";charset=utf8", $config["user"], $config["password"]);
         }catch(Exception $e){
             echo $e->getMessage();
         }
     }
-    public function getCompetence(){
-        $sql = 'SELECT id,domaine, designation FROM Competence';
+    public function getBareme(){
+        $sql = 'SELECT id,designation, points FROM typenote';
         $req = $this->pdo->prepare($sql);
         $req->execute();
         return $req->fetchAll();
